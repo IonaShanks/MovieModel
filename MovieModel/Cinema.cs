be7bb0e7896174cbdf34b53ef4de8f8277f9b638
Ona <Ona@ONA-PC>
2016-11-18 01:00:23 +0000

    Add project files.

---
+++ MovieModel/Cinema.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+
+namespace MovieModel
+{
+    public class Cinema
+    {
+        [Key]
+        public int CinemaID { get; set; }
+        public String Name { get; set; }
+        [Url]
+        public String Website { get; set; }
+        public String PhoneNumber { get; set; }
+        public String TicketPrice { get; set; }
+        public double RunTime { get; set; }
+    }
+
+    //public class MovieContext : DbContext
+    //{
+    //    public MovieContext() : base("DefaultConnection")
+    //    {
+
+    //    }
+    //    public DbSet<Cinema> Cinemas { get; set; }
+    //}
+
+
+}
---
+++ MovieModel/Listing.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+
+
+namespace MovieModel
+{
+    public enum Certification
+    {
+        [Display(Name = "G")]
+        G,
+        [Display(Name = "PG")]
+        PG,
+        [Display(Name = "12")]
+        Twelve,
+        [Display(Name = "12A")]
+        TwelveA,
+        [Display(Name = "15")]
+        Fifteen,
+        [Display(Name = "18")]
+        Eighteen
+    };
+    public class Listing
+    {
+        //Film ID title certification genre description running time
+        [Key]
+        public int FilmID { get; set; }
+        public String Title { get; set; }
+        public Certification Certification { get; set; }
+        public String Genre { get; set; }
+        public String Description { get; set; }
+        public double RunTime { get; set; }
+
+    }
+
+    //public class MovieContext : DbContext
+    //{
+    //    public MovieContext() : base("DefaultConnection")
+    //    {
+
+    //    }
+    //    public DbSet<Listing> Listings { get; set; }
+    //}
+}
+
