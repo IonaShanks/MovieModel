bedecac459982e68935888c7f86b550c327ec2e6
IonaShanks <ionashanks@outlook.com>
2016-11-22 21:12:34 +0000

    Added UnitTest Project

---
+++ MovieModelTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MovieModel;
+
+namespace MovieModelTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            MovieListing m1 = new MovieListing() {FilmID = "HP1", Title = "Harry potter 1", Certification = Certification.PG, Description = "fjeiejfij" , Genre = "Fantasy", RunTime = 198 };
+
+            Console.WriteLine(m1);
+        }
+    }
+}
